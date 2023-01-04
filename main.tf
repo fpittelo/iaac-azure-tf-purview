4e0c9ccd691ff63e05801d93f3c6b8ef60116728
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2023-01-04 11:19:32 +0100

    AB#97 Create a Purview account

--- main.tf
+++ main.tf
@@ -4,6 +4,31 @@
 #
 
 resource "azurerm_resource_group" "rg" {
-  name                = var.rg_name
-  location            = var.rg_location
+  name     = var.rg_name
+  location = var.rg_location
+
+  tags = {
+    department  = var.vm_dpt_it
+    environment = var.vm_env_test
+    owner       = var.vm_owner
+  }
+}
+
+resource "azurerm_purview_account" "iaac_data" {
+  name                        = "iaac-data"
+  resource_group_name         = var.rg_name
+  location                    = var.rg_location
+  public_network_enabled      = true
+  managed_resource_group_name = "iaac-purview-mrgc"
+  depends_on                  = [azurerm_resource_group.rg]
+
+  tags = {
+    department  = var.vm_dpt_it
+    environment = var.vm_env_test
+    owner       = var.vm_owner
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,36 @@
 #
-#All variables used on this project
+# All variables used on this project
 #
 
 # Azure Resource Group Name for the project
 variable "rg_name" {
-  default          = "iaac-azure-tf-purview"
-  type             = string
-  description      = "Azure Resource Group Name for the project"
+  default     = "iaac-azure-tf-purview"
+  type        = string
+  description = "Azure Resource Group Name for the project"
 }
 
 # Variable defining the resource location
 variable "rg_location" {
-  default          = "Switzerland North"
-  type             = string
-  description      = "Azure Resource Group location for all items"
+  default     = "Switzerland North"
+  type        = string
+  description = "Azure Resource Group location for all items"
 }
+
+# Variable for department
+variable "vm_dpt_it" {
+  type    = string
+  default = "IT"
+}
+
+# Variable for test environment
+variable "vm_env_test" {
+  type    = string
+  default = "Test"
+}
+
+# Variable for owner
+variable "vm_owner" {
+  type    = string
+  default = "Fred"
+}
+
