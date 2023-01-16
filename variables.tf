0767490057604f831df82d1e12bf4c83107fdd03
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2023-01-16 21:32:16 +0100

    AB#96 correction

--- variables.tf
+++ variables.tf
@@ -53,7 +53,7 @@ variable "vm_owner" {
 
 # Azure Resource Group Name for the project
 variable "rg_name" {
-  default     = "iaac-data"
+  default     = "azure-tf-purview"
   type        = string
   description = "Azure Resource Group Name for the project"
 }
